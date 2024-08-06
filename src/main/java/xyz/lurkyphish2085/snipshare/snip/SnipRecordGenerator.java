bbd1359cbd7a4a363271c0a0afd0ddd40506b2ff
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-08-06 21:14:11 +0800

    Implement SnipRecordGenerator

---
+++ src/main/java/xyz/lurkyphish2085/snipshare/snip/SnipRecordGenerator.java
@@ -0,0 +1,37 @@
+package xyz.lurkyphish2085.snipshare.snip;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.time.LocalDate;
+
+@Component
+public class SnipRecordGenerator {
+
+    public static final String FILE_NAME_PREFIX = "snip.";
+    public static final String FILE_NAME_EXTENSION = ".txt";
+    public static final int RETRIEVAL_ID_LENGTH = 8;
+
+    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+
+    private final SecureRandom random = new SecureRandom();
+
+    public Snip generate(LocalDate expiryDate) {
+        String id = generateRandomId();
+        String fileName = FILE_NAME_PREFIX + id + FILE_NAME_EXTENSION;
+
+        return new Snip(id, fileName, expiryDate);
+    }
+
+    private String generateRandomId() {
+        StringBuilder generatedId = new StringBuilder();
+
+        for (int i = 0; i < RETRIEVAL_ID_LENGTH; i++) {
+            int randomIndex = random.nextInt(RETRIEVAL_ID_LENGTH);
+            int randomCharacter = ALPHA_NUMERIC_STRING.charAt(randomIndex);
+            generatedId.append(randomCharacter);
+        }
+
+        return generatedId.toString();
+    }
+}
