990abe563946dea759d5209f3f94f90a7dcb2eb7
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-08-06 17:25:49 +0800

    Initial Commit

---
+++ src/main/java/xyz/lurkyphish2085/snipshare/SnipShareApplication.java
@@ -0,0 +1,13 @@
+package xyz.lurkyphish2085.snipshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/lurkyphish2085/snipshare/SnipShareApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.lurkyphish2085.snipshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
