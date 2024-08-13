acba874b8de30c07f535d8196e02fa6a3d1f6878
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-08-13 17:11:57 +0800

    Add isDisposable property on Snip DTOs

--- src/main/java/xyz/lurkyphish2085/snipshare/snip/dto/SnipDTO.java
+++ src/main/java/xyz/lurkyphish2085/snipshare/snip/dto/SnipDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public record SnipDTO(
         String content,
+        Boolean isDisposable,
         LocalDate createdAt,
         LocalDate expiryDate
 ) {
--- src/main/java/xyz/lurkyphish2085/snipshare/snip/dto/SnipSubmissionRequest.java
+++ src/main/java/xyz/lurkyphish2085/snipshare/snip/dto/SnipSubmissionRequest.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public record SnipSubmissionRequest(
         String content,
+        Boolean isDisposable,
         LocalDate expiryDate
 ) {
 }
