623589eae30f06274da7c23d9b3d26991c4279f0
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-10-13 14:04:27 +0800

    Add initialization of sample app users in database

--- src/main/java/xyz/lurkyphish2085/snipshare/SnipShareApplication.java
+++ src/main/java/xyz/lurkyphish2085/snipshare/SnipShareApplication.java
@@ -1,13 +1,36 @@
 package xyz.lurkyphish2085.snipshare;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import xyz.lurkyphish2085.snipshare.user.AppUser;
+import xyz.lurkyphish2085.snipshare.user.AppUserRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SnipShareApplication {
+
+	private final AppUserRepository appUserRepository;
+
+	public SnipShareApplication(AppUserRepository appUserRepository) {
+		this.appUserRepository = appUserRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnipShareApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner testUsers(PasswordEncoder passwordEncoder) throws Exception {
+		return (args) -> {
+			// Add test app users and save these to db
+			AppUser user = new AppUser("user", passwordEncoder.encode("password"), "USER");
+			AppUser admin = new AppUser("admin", passwordEncoder.encode("admin"), "ADMIN");
+
+			appUserRepository.saveAll(List.of(user, admin));
+		};
+	}
 }
