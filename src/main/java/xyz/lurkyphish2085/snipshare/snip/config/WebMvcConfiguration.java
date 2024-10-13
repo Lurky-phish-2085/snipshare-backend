48f62a1050bd231185369dfd999a18b6d3482923
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-10-13 16:45:15 +0800

    Add the Authorization header in the exposedHeaders of cors

--- src/main/java/xyz/lurkyphish2085/snipshare/snip/config/WebMvcConfiguration.java
+++ src/main/java/xyz/lurkyphish2085/snipshare/snip/config/WebMvcConfiguration.java
@@ -2,11 +2,12 @@ package xyz.lurkyphish2085.snipshare.snip.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Value("${client.url}")
     private String clientURL;
@@ -15,10 +16,13 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
-            .allowedOrigins(clientURL) // Adjust this to your frontend URL
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Adjust as needed
+            .allowedOrigins(clientURL)
+            .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true)
-            .exposedHeaders("Location");
+            .exposedHeaders(
+                    HttpHeaders.AUTHORIZATION,
+                    HttpHeaders.LOCATION
+            );
     }
 }
