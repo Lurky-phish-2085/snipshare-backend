1a4f86eb0208c46137d1788fbb6c8a9de191f812
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-10-13 16:21:24 +0800

    Configure Security to allow POST request to login endpoint

--- src/main/java/xyz/lurkyphish2085/snipshare/snip/config/SecurityConfiguration.java
+++ src/main/java/xyz/lurkyphish2085/snipshare/snip/config/SecurityConfiguration.java
@@ -2,17 +2,17 @@ package xyz.lurkyphish2085.snipshare.snip.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import xyz.lurkyphish2085.snipshare.UserDetailsServiceImpl;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -34,13 +34,15 @@ public class SecurityConfiguration {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf((csrf) -> csrf.disable())
-            .httpBasic(withDefaults())
+            .sessionManagement((session) -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
             .authorizeHttpRequests((authorize) -> {
                 authorize
-                    .requestMatchers("/**")
+                    .requestMatchers(HttpMethod.POST, "/login")
                         .permitAll()
                     .anyRequest()
-                        .permitAll();
+                        .authenticated();
             });
 
         return http.build();
