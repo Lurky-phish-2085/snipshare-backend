9dbb9a7409186a3181f9b77bbefab8bfd43ae215
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-10-14 17:26:44 +0800

    Add CurrentAuthor annotation
    This serves as a meta annotation for the CurrentSecurityContext
    annotation which has a SpEL for retrieving an authenticated user's name

---
+++ src/main/java/xyz/lurkyphish2085/snipshare/CurrentAuthor.java
@@ -0,0 +1,14 @@
+package xyz.lurkyphish2085.snipshare;
+
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.PARAMETER, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@CurrentSecurityContext(expression="authentication.name")
+public @interface CurrentAuthor {
+}
