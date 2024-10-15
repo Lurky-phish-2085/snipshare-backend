4fb7f1e628e310d0f372a10ff0a9ee0c00790314
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-10-15 12:12:42 +0800

    Restrict DELETE request to users with roles only
    Public users should never delete any snips.

--- src/main/java/xyz/lurkyphish2085/snipshare/snip/config/SecurityConfiguration.java
+++ src/main/java/xyz/lurkyphish2085/snipshare/snip/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package xyz.lurkyphish2085.snipshare.snip.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -13,7 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import xyz.lurkyphish2085.snipshare.AuthenticationEntryPointImpl;
-import xyz.lurkyphish2085.snipshare.AuthenticationFilter;
+import xyz.lurkyphish2085.snipshare.JwtAuthenticationFilter;
 import xyz.lurkyphish2085.snipshare.UserDetailsServiceImpl;
 
 @Configuration
@@ -21,12 +22,12 @@ import xyz.lurkyphish2085.snipshare.UserDetailsServiceImpl;
 public class SecurityConfiguration {
 
     private final UserDetailsServiceImpl userDetailsService;
-    private final AuthenticationFilter authenticationFilter;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationEntryPointImpl exceptionHandler;
 
-    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService, AuthenticationFilter authenticationFilter, AuthenticationEntryPointImpl exceptionHandler) {
+    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService, JwtAuthenticationFilter authenticationFilter, AuthenticationEntryPointImpl exceptionHandler) {
         this.userDetailsService = userDetailsService;
-        this.authenticationFilter = authenticationFilter;
+        this.jwtAuthenticationFilter = authenticationFilter;
         this.exceptionHandler = exceptionHandler;
     }
 
@@ -45,12 +46,14 @@ public class SecurityConfiguration {
             )
             .authorizeHttpRequests((authorize) -> {
                 authorize
+                    .requestMatchers(HttpMethod.DELETE, "/api/v1/snip/*")
+                        .hasAnyRole("ADMIN", "USER")
                     .requestMatchers("/login", "/api/v1/snip/**")
                         .permitAll()
                     .anyRequest()
                         .authenticated();
             })
-            .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .exceptionHandling((handler) -> handler
                     .authenticationEntryPoint(exceptionHandler)
             );
