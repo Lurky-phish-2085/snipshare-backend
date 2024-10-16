08f561beeb06ecae6d1252dbf039c40d3b78bf3e
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-10-17 07:50:05 +0800

    Add register endpoint with rudimentary impl

--- src/main/java/xyz/lurkyphish2085/snipshare/auth/AuthenticationController.java
+++ src/main/java/xyz/lurkyphish2085/snipshare/auth/AuthenticationController.java
@@ -5,11 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.lurkyphish2085.snipshare.common.RestEndpoints;
+import xyz.lurkyphish2085.snipshare.user.AppUser;
+import xyz.lurkyphish2085.snipshare.user.AppUserRepository;
 
 @RestController
 @RequestMapping(path = RestEndpoints.AUTHENTICATION)
@@ -17,14 +20,18 @@ public class AuthenticationController {
 
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final AppUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthenticationController(JwtService jwtService, AuthenticationManager authenticationManager) {
+    public AuthenticationController(JwtService jwtService, AuthenticationManager authenticationManager, AppUserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.jwtService = jwtService;
         this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> getToken(@RequestBody AccountCredentials credentials) {
+    public ResponseEntity<?> login(@RequestBody AccountCredentials credentials) {
         UsernamePasswordAuthenticationToken creds = new
                 UsernamePasswordAuthenticationToken(
                     credentials.username(),
@@ -42,5 +49,23 @@ public class AuthenticationController {
                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwts)
                    .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization")
                    .build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody AccountCredentials credentials) {
+        userRepository.findByUsername(credentials.username())
+            .ifPresent(user -> {
+                throw new IllegalArgumentException("User already exists.");
+            });
+
+        AppUser registeredUser =
+                new AppUser(
+                        credentials.username(),
+                        passwordEncoder.encode(credentials.password()),
+                        Roles.USER.name()
+                );
+        userRepository.save(registeredUser);
+
+        return ResponseEntity.ok().build();
     }
 }
